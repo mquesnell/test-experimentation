5e5a9b86e490ddbfda82d45f3d754dc6fece8197
Mike Quesnell <michael.quesnell@us.af.mil>
2021-03-17 08:39:39 -0600

    Initial commit -- working jest runner -- basic MUI app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/** @format */
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test("show the dom", () => {
+    screen.debug();
+  });
+
+  test("renders title", () => {
+    const linkElement = screen.getByText(
+      "Example Material-UI/React Application"
+    );
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders the ok button", () => {
+    const buttonEl = screen.getByText("ok");
+    expect(buttonEl).toBeInTheDocument();
+  });
 });
